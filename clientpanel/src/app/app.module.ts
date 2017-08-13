3d5b024df5a2977da99df5de807f8bdf6819d6d6
sandeepchunduru <schunduru24@gmail.com>
2017-08-12 23:36:50 -0400

    first commit

---
+++ clientpanel/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes} from '@angular/router';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
